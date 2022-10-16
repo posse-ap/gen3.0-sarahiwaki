21f281fdf100d1652c2ff8ce8c0e3019f130d0a8
“岩木さら” <sarahiwaki@keio.jp>
2022-10-16 14:29:06 +0900

    バグ修正

--- quiz-page/quiz-page copy.js
+++ quiz-page/quiz-page copy.js
@@ -8,73 +8,71 @@
 
 
 
-
-
-//問題の内容まとめ
-  let questions = [
-    {
-      index: 1,
-      question: '日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？',
-      img: '../assets-ph1-website-main/img/quiz/img-quiz01.png',
-      option1: '約28万人',
-      option2: '約79万人',
-      option3: '約183万人',
-      reference: '経済産業省 2019年3月 － IT 人材需給に関する調査',
-    },
-
-
-    {
-      index: 2,
-      question: '既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？',
-      img: '../assets-ph1-website-main/img/quiz/img-quiz02.png',
-      option1: 'INTECH',
-      option2: 'BIZZTECH',
-      option3: 'X-TECH',
-      reference: ""
-    },
-
-
-    {
-      index: 3,
-      question: 'IoTとは何の略でしょう？',
-      img: '../assets-ph1-website-main/img/quiz/img-quiz03.png',
-      option1: 'Internet of Things',
-      option2: 'Integrate into Technology',
-      option3: 'Information  on Tool',
-      reference: ""
-    },
-
-    {
-      index: 4,
-      question: '日本が目指すサイバー空間とフィジカル空間を高度に融合させたシステムによって開かれる未来社会のことをなんと言うでしょうか？',
-      img: '../assets-ph1-website-main/img/quiz/img-quiz04.png',
-      option1: 'Society 5.0',
-      option2: 'CyPhy',
-      option3: 'SDGs',
-      reference: 'Society5.0 - 科学技術政策 - 内閣府',
-    },
-
-    {
-      index: 5,
-      question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
-      img: '../assets-ph1-website-main/img/quiz/img-quiz05.png',
-      option1: 'Web3.0',
-      option2: 'NFT',
-      option3: 'メタバース',
-      reference: ""
-    },
-
-    {
-      index: 6,
-      question: '先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？',
-      img: '../assets-ph1-website-main/img/quiz/img-quiz06.png',
-      option1: '約2倍',
-      option2: '約5倍',
-      option3: '約11倍',
-      reference: 'Accenture Technology Vision 2021',
-    }
-  ]
-
+const questions = [
+  {
+    id: 1,
+    question: '日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？',
+    answers: ['約28万人', '約79万人', '約183万人'],
+    correctNumber: 1,
+    reference: '経済産業省 2019年3月 － IT 人材需給に関する調査',
+    img: '../assets-ph1-website-main/img/quiz/img-quiz01.png',
+    option1: '約28万人',
+    option2: '約79万人',
+    option3: '約183万人',
+  },
+  {
+    id: 2,
+    question: '既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？',
+    answers: ['INTECH', 'BIZZTECH', 'X-TECH'],
+    correctNumber: 2,
+    img: '../assets-ph1-website-main/img/quiz/img-quiz02.png',
+    option1: 'INTECH',
+    option2: 'BIZZTECH',
+    option3: 'X-TECH',
+  },
+  {
+    id: 3,
+    question: 'IoTとは何の略でしょう？',
+    answers: ['Internet of Things', 'Integrate into Technology', 'Information on Tool'],
+    correctNumber: 0,
+    img: '../assets-ph1-website-main/img/quiz/img-quiz03.png',
+    option1: 'Internet of Things',
+    option2: 'Integrate into Technology',
+    option3: 'Information  on Tool',
+  },
+  {
+    id: 4,
+    question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
+    answers: ['Society 5.0', 'CyPhy', 'SDGs'],
+    correctNumber: 0,
+    reference: 'Society5.0 - 科学技術政策 - 内閣府',
+    img: '../assets-ph1-website-main/img/quiz/img-quiz04.png',
+    option1: 'Society 5.0',
+    option2: 'CyPhy',
+    option3: 'SDGs',
+  },
+  {
+    id: 5,
+    question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
+    answers: ['Web3.0', 'NFT', 'メタバース'],
+    correctNumber: 0,
+    img: '../assets-ph1-website-main/img/quiz/img-quiz05.png',
+    option1: 'Web3.0',
+    option2: 'NFT',
+    option3: 'メタバース',
+  },
+  {
+    id: 6,
+    question: '先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？',
+    answers: ['約2倍', '約5倍', '約11倍'],
+    correctNumber: 1,
+    reference: 'Accenture Technology Vision 2021',
+    img: '../assets-ph1-website-main/img/quiz/img-quiz06.png',
+    option1: '約2倍',
+    option2: '約5倍',
+    option3: '約11倍',
+  }
+];
 
 
 
@@ -84,7 +82,7 @@ for (let i = 0; i< questions.length; i++) {
   let element = document.getElementById('box');
 
   let text = `<div class="q1">
-  <div class="q1-content js-quiz" data-quiz="${questions[i].index-1}">
+  <div class="q1-content js-quiz" data-quiz="0">
     <p class="q1-heading">Q${questions[i].index}</p>
     <div class="q1-heading-content">
       ${questions[i].question}
@@ -124,7 +122,6 @@ for (let i = 0; i< questions.length; i++) {
       <p class="js-answerText"></p>
     </div>
   
-    
     <div class="q1-reference">  
       <img
         src="../assets-ph1-website-main/img/icon/icon-note.svg"
@@ -136,17 +133,8 @@ for (let i = 0; i< questions.length; i++) {
   </div>
   </div>`;
   // };
-
-  // const reference = document.querySelectorAll('.q1-reference');
-  // const displaynote = note => {
-  // questions.reference
-  // }
-  const note = document.querySelectorAll('.q1-reference');
-// if(questions[i].reference === null){
-  note.style.display = "none";
-// }
-
-console.log(note)
+  
+  
  element.insertAdjacentHTML('beforebegin', text);
 };
 
@@ -175,33 +163,8 @@ const shuffleQuiz = ([...array]) => {
     return array;
   }
 
-  // 回答一覧
-  const CORRECT_ANSWERS = [
-    {
-      index: 1,
-      value: "約79万人",
-    },
-    {
-      index: 2,
-      value: "X-TECH",
-    },
-    {
-      index: 0,
-      value: "Internet of Things",
-    },
-    {
-      index: 0,
-      value: "Society 5.0",
-    },
-    {
-      index: 0,
-      value: "Web3.0",
-    },
-    {
-      index: 1,
-      value: "約5倍",
-    },
-  ];
+  // const quizArray = shuffle(questions)
+
 
   // すべての問題を取得
   const allQuiz = document.querySelectorAll(".js-quiz");
